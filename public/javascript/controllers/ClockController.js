37c603b28d5699a1d613673ea604a3ab96a3e158
M-Doja <micdoja@yahoo.com>
2015-12-24 16:11:46 -0800

    push recent changes

--- public/javascript/controllers/ClockController.js
+++ public/javascript/controllers/ClockController.js
@@ -14,40 +14,37 @@
     vm.count;
     vm.setClock;
     vm.setBreak = null;
-    vm.minutes = Math.floor(vm.count / 60);
-    vm.seconds = Math.floor(vm.count % 60);
-    clearInterval(vm.countdown);
-    vm.countdown = setInterval(function() {
-      if (vm.count > 59) {
-        if (vm.seconds < 10 && vm.seconds >= 0) {
-          vm.seconds = '0' + vm.seconds;
-        }
-        else if (vm.seconds < 0 ) {
-          vm.minutes--;
-          vm.seconds = vm.count % 60;
-        }
-        document.getElementById('display').innerHTML += vm.minutes + ":" + seconds + ' remaining!';
-        vm.seconds--;
-      }
-      if (vm.count < 60 && vm.count >= 0) {
-        document.getElementById('display').innerHTML += vm.count + " seconds remaining!";
-        if (vm.count === 0) {
-          // alert(alert_text); // alert text goes here
-          document.getElementById('display').innerHTML = '';
-        }
-      }
-      vm.count--; // you only need this once
-      return;
-    }, 1000);
+    // vm.minutes = Math.floor(vm.count / 60);
+    // vm.seconds = Math.floor(vm.count % 60);
+    // clearInterval(vm.countdown);
+    // vm.countdown = setInterval(function() {
+    //   if (vm.count > 59) {
+    //     if (vm.seconds < 10 && vm.seconds >= 0) {
+    //       vm.seconds = '0' + vm.seconds;
+    //     }
+    //     else if (vm.seconds < 0 ) {
+    //       vm.minutes--;
+    //       vm.seconds = vm.count % 60;
+    //     }
+    //     document.getElementById('display').innerHTML += vm.minutes + ":" + seconds + ' remaining!';
+    //     vm.seconds--;
+    //   }
+    //   if (vm.count < 60 && vm.count >= 0) {
+    //     document.getElementById('display').innerHTML += vm.count + " seconds remaining!";
+    //     if (vm.count === 0) {
+    //       // alert(alert_text); // alert text goes here
+    //       document.getElementById('display').innerHTML = '';
+    //     }
+    //   }
+    //   vm.count--; // you only need this once
+    //   return;
+    // }, 1000);
 
 
 
     vm.setClockTimer = function() {
-      if (vm.setClock !== 0) {
-        for (var i = 0; i < 30; i++) {
-        document.getElementById('display').innerHTML += [i];
-      }
-     }
+    vm.timer = 5;
+    
     }
    vm.delayedAlert = function() {
     vm.timerID = window.setInterval(vm.setClockTimer, 1000);
